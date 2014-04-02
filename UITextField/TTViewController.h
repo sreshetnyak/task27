5e4454b4b6d652d521116832bf7e41f1dd66e8b5
sreshetnyak <reshetniak.sergey@gmail.com>
2014-04-02 11:48:26 +0300

    Initial commit from local git

---
+++ UITextField/TTViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTViewController.h
+//  UITextField
+//
+//  Created by sergey on 4/2/14.
+//  Copyright (c) 2014 sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTViewController : UIViewController
+
+@end
---
+++ UITextField/TTViewController.m
@@ -0,0 +1,29 @@
+//
+//  TTViewController.m
+//  UITextField
+//
+//  Created by sergey on 4/2/14.
+//  Copyright (c) 2014 sergey. All rights reserved.
+//
+
+#import "TTViewController.h"
+
+@interface TTViewController ()
+
+@end
+
+@implementation TTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
